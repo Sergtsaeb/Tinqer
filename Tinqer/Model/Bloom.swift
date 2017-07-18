860d2cbf02a4a62c9d6a1d0a0f2e8c4cdfdf0337
Serg T <sergtsaeb@gmail.com>
2017-07-18 14:59:49 -0700

    personalVC tableview delegates

---
+++ Tinqer/Model/Bloom.swift
@@ -0,0 +1,13 @@
+//
+//  Bloom.swift
+//  Tinqer
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 7/18/17.
+//  Copyright © 2017 Sergtsaeb. All rights reserved.
+//
+
+import Foundation
+
+class Bloom {
+    
+}
