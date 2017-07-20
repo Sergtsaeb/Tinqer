347488f98f28d3f38346e5aa6d395caa64707fd0
Serg T <sergtsaeb@gmail.com>
2017-07-20 01:07:01 -0700

    bloom swipe animations

--- Tinqer/Controller/SwipeViewController.swift
+++ Tinqer/Controller/SwipeViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class SwipeViewController: UIViewController {
-    
 
     @IBOutlet weak var swipeImageView: UIImageView!
+    @IBOutlet weak var card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
+    
+    
     
     
     @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
@@ -71,15 +73,44 @@ class SwipeViewController: UIViewController {
          
         
         if sender.state == UIGestureRecognizerState.ended {
+            
+            if card.center.x < 75 {
+                //move to the left
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+                
+            } else if card.center.x > (view.frame.width - 75) {
+                //move to the right
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+            }
+            
             //reset center to middle once the pan ends
             UIView.animate(withDuration: 0.2) {
                 card.center = self.view.center
+                self.swipeImageView.alpha = 0
             }
         }
-        
-        
+     
         
     }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        //reset center to middle once the pan ends
+        UIView.animate(withDuration: 0.2) {
+            self.card.center = self.view.center
+            self.swipeImageView.alpha = 0
+            self.card.alpha = 1
+        }
+    }
+    
+   
     
 }
 
