d86dd05a6a361457a3f06c5b9e74b9e31608dcb5
0utlier <jdsqrpnts@yahoo.com>
2017-07-28 15:32:46 -0700

    label setup

--- Tinqer/ComposeViewController.swift
+++ Tinqer/ComposeViewController.swift
@@ -14,12 +14,29 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
         super.viewDidLoad()
         
         print("The ComposeVC opened!")
-        //make the keyboard appear immediately. right into composing [first responder]
         super.viewDidLoad()
         bloomField.delegate = self
         self.characterCountLabel.text = "140"
+        labelSetUp(for: categoryLabel, color: UIColor.orange)
+        labelSetUp(for: attachLabel, color: UIColor.blue)
+        
+        //make the keyboard appear immediately. right into composing [first responder]
+        bloomField.becomeFirstResponder()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
     }
-
+    
+    func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+//            bloomField.frame.origin.y = keyboardHeight - bloomField.frame.height - attachLabel.frame.height
+//            print(keyboardHeight / self.view.frame.height)
+//            print(bloomField.frame.origin.y)
+            
+        }
+    }
+    
     // PRAGMA MARK: OUTLETS
     // Outlets
     @IBOutlet weak var bloomField: UITextView!
@@ -28,37 +45,29 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var attachLabel: UILabel!
     
-    
+    // PRAGMA MARK: LABEL SETUP
+    func labelSetUp(for label:UILabel, color:UIColor) {
+        label.backgroundColor = color
+    }
     
     // PRAGMA MARK: TEXTVIEW DELEGATES
     
-    
-    /*
- func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        guard let text = textField.text else { return true }
-        let length = text.characters.count + string.characters.count - range.length
-        
-        // create an Integer of 55 - the length of your TextField.text to count down
-        let count = 140 - length
-        
-        // set the .text property of your UILabel to the live created String
-        characterCountLabel.text =  String(count)
-        
-        // if you want to limit to 55 characters
-        // you need to return true and <= 55
-        
-        return length <= 140 // To just allow up to 55 characters
-    }
- */
     // function to count characters and limit composer
-
+    
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
         let numberOfChars = 140 - Int(newText.characters.count)
-//        let countRemain = 140 - Int(numberOfChars)
+        //        let countRemain = 140 - Int(numberOfChars)
         self.characterCountLabel.text = "\(numberOfChars)"
         return numberOfChars < 140 // if the desire is to STOP the user from going past 140, remove "140 - Int(... from numberOfChars and uncomment coutnRemain"
+    }
+    
+    /*
+     func textViewDidBeginEditing(_ textView: UITextView) {
+        if bloomField.textColor == UIColor.black {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
     }
     
     override func becomeFirstResponder() -> Bool {
@@ -66,7 +75,7 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
         
         return true
     }
-    
+    */
     
     
     /* TO BE DONE
@@ -74,7 +83,7 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
      pull down on category (dismiss keyboard) (open respective page)
      pull up on attach (dismiss keyboard) (open respective page)
      */
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
